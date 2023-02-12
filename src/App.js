fb2194b44b9fe7d79de3aaba4540e2b6620b4b2b
Joel Deon Dsouza <oka.welt.dev@gmail.com>
2023-02-12 12:14:43 +0100

    added response frontend components and styles

--- src/App.js
+++ src/App.js
@@ -12,6 +12,37 @@ function App() {
       </aside>
       {/* Search Section */}
       <section className="search-container">
+        <div className="response-log">
+          <div className="response-massage">
+            <div className="response-massage-center">
+              <div className="avatar">
+              </div>
+              <div className="message">
+                Hello World
+                </div>
+            </div>
+          </div>
+          <div className="response-massage ai-response">
+            <div className="response-massage-center">
+              <div className="avatar ai-response">
+                <svg
+                  viewBox="0 0 24 24"
+                  fill="currentColor"
+                  height="2.5em"
+                  width="2.6em"
+                >
+                  <path d="M21.928 11.607c-.202-.488-.635-.605-.928-.633V8c0-1.103-.897-2-2-2h-6V4.61c.305-.274.5-.668.5-1.11a1.5 1.5 0 00-3 0c0 .442.195.836.5 1.11V6H5c-1.103 0-2 .897-2 2v2.997l-.082.006A1 1 0 001.99 12v2a1 1 0 001 1H3v5c0 1.103.897 2 2 2h14c1.103 0 2-.897 2-2v-5a1 1 0 001-1v-1.938a1.006 1.006 0 00-.072-.455zM5 20V8h14l.001 3.996L19 12v2l.001.005.001 5.995H5z" />
+                  <path d="M10 12 A1.5 2 0 0 1 8.5 14 A1.5 2 0 0 1 7 12 A1.5 2 0 0 1 10 12 z" />
+                  <path d="M17 12 A1.5 2 0 0 1 15.5 14 A1.5 2 0 0 1 14 12 A1.5 2 0 0 1 17 12 z" />
+                  <path d="M8 16h8v2H8z" />
+                </svg>
+              </div>
+              <div className="message">
+                Hello From AI
+                </div>
+            </div>
+          </div>
+        </div>
         <div className="search-input-container">
           <input rows="1" className="search-input-text-field" placeholder="Scoop here" />
         </div>
